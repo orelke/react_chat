a0435bc3e920fe7a68619ee9a4e9b4c089a26efc
orel <orel.keliahu@mail.huji.ac.il>
2020-05-09 16:32:00 +0300

    finish add

--- components/Add.js
+++ components/Add.js
@@ -4,16 +4,24 @@ import { Searchbar } from 'react-native-paper'
 import  firebase from 'firebase';
 
 export default class Add extends Component {
+    constructor(props){
+      super(props)
+      this.already_request_sent = this.already_request_sent.bind(this)
+      this.already_friend = this.already_friend.bind(this)
+
+    }
 
     state = {
-       loading_search: true,
-       search: '',
-       reciver_name: '',
-       reciver_id: '',
-       loading_add: true,
+      already_friend : false,
+      already_sent_request : false,
+      loading_search: true,
+      search: '',
+      reciver_name: '',
+      reciver_id: '',
+      loading_add: true,
     };
 
-
+   
     find_name =  async () =>
     {
         var store_ref = firebase.firestore().collection('Users').get().then(snapshot => {
@@ -57,10 +65,78 @@ export default class Add extends Component {
 
     }
 
+    cancel_friendship = () => 
+    {
+      var user_id = firebase.auth().currentUser.uid;
+      var reciver_id = this.state.reciver_id
+      var sender_doc_ref = firebase.firestore().collection('Users').doc(user_id)
+      sender_doc_ref.set({ "friends_list" : {[reciver_id]: firebase.firestore.FieldValue.delete()}}, { merge: true });
+      
+      var reciver_doc_ref = firebase.firestore().collection('Users').doc(reciver_id)
+      reciver_doc_ref.set({ "friends_list" : {[user_id]: firebase.firestore.FieldValue.delete()}}, { merge: true });
+
+    }
+
+
+    already_request_sent = () => 
+    {
+      var user_id = firebase.auth().currentUser.uid
+      var reciver_id =  this.state.reciver_id
+      var sender_doc = firebase.firestore().collection('Users').doc(user_id)
+      var result = false
+
+    sender_doc.get().then(function(doc) {
+    if (doc.exists) {
+        var doc_data = doc.data()
+        console.log(reciver_id, "rid")
+        console.log("IS sent!",reciver_id in doc_data.sent_requests)
+        result = reciver_id in doc_data.sent_requests  
+        console.log("result value", result)
+      
+        
+
+    } else {
+        console.log("No such document!");
+    }
+    }).catch(function(error) {
+    console.log("Error getting document:", error);
+    });
+    
+    return result
+
+    }
+
+
+
+    already_friend = () => 
+    {
+      var result = false
+      var user_id = firebase.auth().currentUser.uid
+      var reciver_id =  this.state.reciver_id
+      var sender_doc = firebase.firestore().collection('Users').doc(user_id)
+
+    sender_doc.get().then(function(doc) {
+    if (doc.exists) {
+        var doc_data = doc.data()
+        result = reciver_id in doc_data.sent_requests
+
+
+    } else {
+        console.log("No such document!");
+    }
+    }).catch(function(error) {
+    console.log("Error getting document:", error);
+    });
+      return result
+
+    }
+
 
 
     render(){
+       
 
+       
         if (this.state.loading_search)
         {
             console.log("XXXXXX")
@@ -69,29 +145,53 @@ export default class Add extends Component {
                 <Searchbar
                 placeholder="Search"
                 onChangeText={query => {this.setState({search:query}, this.find_name)}}
-                onSubmitEditing={()=> this.setState({loading_search:false})}
+                onSubmitEditing={()=> {this.setState({loading_search:false}, this.check_status)}}
                 value={this.state.search}
 
                 /> 
             </View>
             ) 
         }
-     
-        else 
+
+        if (this.state.reciver_name === '')
         {
-          if (this.state.reciver_name === '')
-          {
             return( <Text> User does not found!</Text>)
 
-          }
-          else
-          {
-              if (this.state.loading_add)
-              {
-              console.log(this.state.loading_add)
+        }
+        console.log("already state", this.already_request_sent())
+        if(this.already_request_sent())
+        { return ( <View>
+                   <Text> 
+                    {this.state.reciver_name} is here! 
+                   </Text>
+                   <Button 
+                   title="CANCEL FRIEND REQUEST"
+                   onPress={query => {this.setState({already_sent_request:true} ,this.cancel_request)}}
+                   />
+                   </View>)
+            
+        }
+
+        if(this.already_friend())
         
-              console.log(this.state.reciver_name, "BLA BLA")
-              return ( <View>
+        { return ( <View>
+                   <Text> 
+                    {this.state.reciver_name} is here! 
+                   </Text>
+                   <Button 
+                   title="CANCEL FRIENDSHIP"
+                   onPress={query => {this.setState({already_friend:false} ,this.cancel_friendship)}}
+                   />
+                   </View>)
+            
+        }
+        else 
+        {
+        if (this.state.loading_add)
+        {
+          console.log(this.state.loading_add)
+          console.log(this.state.reciver_name, "BLA BLA")
+          return ( <View>
                    <Text> 
                     {this.state.reciver_name} is here! 
                    </Text>
@@ -100,10 +200,10 @@ export default class Add extends Component {
                    onPress={query => {this.setState({loading_add:false} ,this.send_request)}}
                    />
                    </View>)
-              }
+          }
 
-              else
-            { return ( <View>
+        else
+          { return ( <View>
                    <Text> 
                     {this.state.reciver_name} is here! 
                    </Text>
@@ -112,16 +212,14 @@ export default class Add extends Component {
                    onPress={query => {this.setState({loading_add:true} ,this.cancel_request)}}
                    />
                    </View>)
-            }
+          }
 
-            
-              
+  
 
           }
         }          
     }
 
- }
 
 
     
